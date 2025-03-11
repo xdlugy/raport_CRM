f3a5536069defde8f9bccd3ecb670bd9518bf6f5
kacper <xdlugy@gmail.com>
2025-03-11 23:25:22 +0100

    login, register views and psql for users

--- routes.php
+++ routes.php
@@ -3,6 +3,14 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch($request) {
     case "/":
-        require_once("./views/login.php");
+        if(!isset($_SESSION['user'])) {
+            require_once("./views/login.php");
+        }
+        else {
+            require_once("./views/home.php");
+        }
+    break;
+    case "/register":
+        require_once("./views/register.php");
 }
 ?>
---
+++ views/register.php
@@ -0,0 +1,9 @@
+<form method="POST" action="/controllers/register-controller.php">
+<input type="text" name="username" placeholder="Username">
+<input type="email" name="email" placeholder="Email">
+<input type="email" name="email_confirm" placeholder="Confirm email">
+<input type="password" name="password" placeholder="Password">
+<button type="submit">Register</button>
+</form>
+
+<a href="/reset-password">Already have an account?</a>
